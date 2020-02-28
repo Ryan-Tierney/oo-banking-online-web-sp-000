f5b5cc08a8c1e14578eb52a0badaa99076c7a491
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-28 05:52:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
   
-  attr_accessor :sender, :receiver, :status
+  attr_accessor :sender, :receiver, :status, :amount
   
   def initialize(sender, reciever)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @amount = 0
   end 
 end
