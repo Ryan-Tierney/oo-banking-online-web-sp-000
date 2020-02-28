42ac1de05540af43ce1fe1da8c7cc1c5333f6b4e
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-28 05:39:53 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,7 +18,7 @@ class BankAccount
     return "Your balance is $#{@balance}."
   end 
   
-  def valid?()
+  def valid?(name, status, balance)
     if @status == open && @balance > 0 
       true 
     else 
